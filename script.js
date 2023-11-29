7193cbb16243dc0881b8b4053900211d37dc5fcc
lis3q . <lis3qx@gmail.com>
2023-11-29 22:41:03 +0100

    I created sections for the best artists, newsletter, footer, sticky navbar and I changed a logo

--- script.js
+++ script.js
@@ -51,31 +51,39 @@ fetch("json/news.json").then(function(response){
 	book.innerHTML = out;
 });
 
+// Displaying sale books from a JSON file
+fetch("json/sale.json").then(function(response){
+	return response.json();
+})
+.then(function(products){
+	let book = document.querySelector("#sale .boxes");
+	let out = "";
+	for(let product of products){
+		out += `
+            <div class="swiper-slide box">
+              <div class="image">
+                  <div class="sale">${product.sale}</div>
+                  <img src="${product.img}" alt="${product.author}/${product.title}">
+                  <div class="fav-btn"><i class="fa-solid fa-heart-circle-plus"></i></div>
+              </div>
+              <div class="title">${product.title}</div>
+                <div class="author">${product.author}</div>
+              <div class="price">${product.price} <span class="discount">${product.discount}</span></div>
+              <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
+              <div class="btn"><button><i class="fa-solid fa-cart-plus"></i> Dodaj do koszyka</button></div>
+          </div>
+		`;
+	}
 
-// Displaying daily book from a JSON file
-// fetch("json/dailybook.json").then(function(response){
-// 	return response.json();
-// })
-// .then(function(products){
-// 	let book = document.querySelector("#dailybook .daily");
-// 	let out = "";
-// 	for(let product of products){
-// 		out += `
-//             <div class="swiper-slide box">
-//               <div class="image">
-//                   <img src="${product.img}" alt="${product.author}/${product.title}">
-//                   <div class="fav-btn"><i class="fa-solid fa-heart-circle-plus"></i></div>
-//               </div>
-//               <div class="title">${product.title}</div>
-//                 <div class="author">${product.author}</div>
-//               <div class="price">${product.price} <span class="discount">${product.discount}</span></div>
-//               <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
-//               <div class="btn"><button><i class="fa-solid fa-cart-plus"></i> Dodaj do koszyka</button></div>
-//           </div>
-// 		`;
-// 	}
+	book.innerHTML = out;
+});
 
-// 	book.innerHTML = out;
-// });
+
+
+// Sticky navbar
+let nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("sticky", window.scrollY > 0)
+})
 
 
