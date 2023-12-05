13ccaf55e2c8fe2602f6c00c85fa3a922e90fc0c
lis3q . <lis3qx@gmail.com>
2023-12-05 13:23:15 +0100

    I added books, contact, all books (json) files

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ fetch("json/recommended.json").then(function(response){
               <div class="title">${product.title}</div>
                 <div class="author">${product.author}</div>
               <div class="price">${product.price} <span class="discount">${product.discount}</span></div>
-              <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
+              <div class="rating">${product.rating}</div>
               <div class="btn"><button><i class="fa-solid fa-cart-plus"></i> Dodaj do koszyka</button></div>
           </div>
 		`;
@@ -57,6 +57,33 @@ fetch("json/sale.json").then(function(response){
 })
 .then(function(products){
 	let book = document.querySelector("#sale .boxes");
+	let out = "";
+	for(let product of products){
+		out += `
+            <div class="swiper-slide box">
+              <div class="image">
+                  <div class="sale">${product.sale}</div>
+                  <img src="${product.img}" alt="${product.author}/${product.title}">
+                  <div class="fav-btn"><i class="fa-solid fa-heart-circle-plus"></i></div>
+              </div>
+              <div class="title">${product.title}</div>
+                <div class="author">${product.author}</div>
+              <div class="price">${product.price} <span class="discount">${product.discount}</span></div>
+              <div class="rating"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
+              <div class="btn"><button><i class="fa-solid fa-cart-plus"></i> Dodaj do koszyka</button></div>
+          </div>
+		`;
+	}
+
+	book.innerHTML = out;
+});
+
+// Displaying all books from a JSON file
+fetch("json/all.json").then(function(response){
+	return response.json();
+})
+.then(function(products){
+	let book = document.querySelector(".books .boxes");
 	let out = "";
 	for(let product of products){
 		out += `
